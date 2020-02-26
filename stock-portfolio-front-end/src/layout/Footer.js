2a6b537e35f8887a4d7bc9e4d6c5a5843ab7c01b
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-02-26 03:47:41 -0500

    added the footer link

---
+++ stock-portfolio-front-end/src/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div style={{ marginTop: '10rem', marginBottom: '1rem'}}>
+      <footer>
+        <a href="https://iexcloud.io">Data provided by IEX Cloud</a>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
