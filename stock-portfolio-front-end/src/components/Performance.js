ed915de7d9dc08465c764944c8d156f9e196baba
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-02-26 03:15:24 -0500

    able to render the colors properly

--- stock-portfolio-front-end/src/components/Performance.js
+++ stock-portfolio-front-end/src/components/Performance.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { connect } from 'react-redux'
 
 function Performance({ allTransactions, getPortfolioTotal }) {
+  const [ allOpeningPrices, setAllOpeningPrices ] = useState(null)
   const tickerTracker = {}
+  const openingPrices = {}
   const uniqueTransactions = []
+  const TOKEN = `Tpk_b0d2d52327d14c40b708b93e14b27f40`
 
+  // For loop was to find all unique stocks
   for (let i = allTransactions.length-1; i >= 0; i--) {
     if (!tickerTracker[allTransactions[i].ticker.toUpperCase()]) {
       tickerTracker[allTransactions[i].ticker] = true
       uniqueTransactions.push(allTransactions[i])
     }
+  }
+
+  const uniqueTickers = uniqueTransactions.map( transaction => transaction.ticker )
+  const symbols = uniqueTickers.join()
+
+  // Unable to fetch information symbols does not exist
+  if (symbols && !allOpeningPrices) {
+    fetch(`https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${symbols}&types=chart&range=1d&token=${TOKEN}`)
+      .then(resp => resp.json())
+      .then(resp => {
+        for (let ticker in resp) {
+          openingPrices[ticker] = resp[ticker].chart[3].open
+        }
+        setAllOpeningPrices(openingPrices)
+      })
   }
 
   let total = uniqueTransactions.reduce( (acc, value) => {
@@ -21,6 +40,19 @@ function Performance({ allTransactions, getPortfolioTotal }) {
 
   const listOfTransactions = uniqueTransactions.map( (transaction, index) => {
     const { ticker, bought_price, shares } = transaction
+    const openingPrice = allOpeningPrices ? allOpeningPrices[ticker] : null
+    const boughtPrice = parseFloat(bought_price)
+    let color
+
+    if (boughtPrice < openingPrice) {
+      color = "red"
+    } else if (boughtPrice === openingPrice) {
+      color = "grey"
+    } else if (boughtPrice > openingPrice) {
+      color = "green"
+    } else {
+      color = "black"
+    }
 
     return (
       <tr key={index}>
@@ -28,19 +60,11 @@ function Performance({ allTransactions, getPortfolioTotal }) {
           style={{
             paddingBottom: '1rem',
             paddingTop: '1rem',
-            borderBottom: '1px solid black'
+            borderBottom: '1px solid black',
+            color: color
           }}
         >
-          {ticker} - {shares} shares
-        </td>
-        <td
-          style={{
-            paddingBottom: '1rem',
-            paddingTop: '1rem',
-            float: 'right',
-            borderBottom: '1px solid black'
-          }}>
-            ${parseFloat(bought_price).toFixed(2)}
+          {ticker} - {shares} shares ${parseFloat(bought_price).toFixed(2)}
         </td>
       </tr>
     )
